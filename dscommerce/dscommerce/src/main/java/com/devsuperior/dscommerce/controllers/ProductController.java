d034a05ca687a13e5d61b68c3074ad2daeb32554
ROBERTA, Karol <karolroberta92@hotmail.com>
2023-12-25 02:15:12 -0300

    build: PUT product

--- dscommerce/dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ dscommerce/dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -6,10 +6,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +25,7 @@ import com.devsuperior.dscommerce.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
+@CrossOrigin("*")
 public class ProductController {
 
 	@Autowired
@@ -34,7 +38,7 @@ public class ProductController {
 	}
 	
 	@GetMapping()
-	public ResponseEntity <Page<ProductDTO>>findAll(Pageable pageable) {
+	public ResponseEntity <Page<ProductDTO>>findAll(@PageableDefault(size=10) Pageable pageable) {
 		Page<ProductDTO> dto = service.findAll(pageable);
 		return ResponseEntity.ok(dto);
 	}
@@ -46,5 +50,14 @@ public class ProductController {
 				.buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
 	}
+	
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity <ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok(dto);
+	}
+	
+
 	
 }
