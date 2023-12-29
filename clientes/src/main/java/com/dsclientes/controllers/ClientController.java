b2eb8d3f6c81ec94d93302b0cf26817a5d07ca96
ROBERTA, Karol <karolroberta92@hotmail.com>
2023-12-29 02:04:35 -0300

    build: GET clients page

--- clientes/src/main/java/com/dsclientes/controllers/ClientController.java
+++ clientes/src/main/java/com/dsclientes/controllers/ClientController.java
@@ -1,8 +1,11 @@
 package com.dsclientes.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +24,13 @@ public class ClientController {
 	
 	@GetMapping(value = "/{id}")
 	public ClientDTO findById(@PathVariable Long id) {
-		return service.findByIdClientDTO(id);
+		return service.findById(id);
+		 
+	}
+	
+	@GetMapping
+	public Page<ClientDTO> findAll(Pageable pageable) {
+		return service.findAll(pageable);
 		 
 	}
 }
--- clientes/src/main/java/com/dsclientes/services/ClientService.java
+++ clientes/src/main/java/com/dsclientes/services/ClientService.java
@@ -1,8 +1,11 @@
 package com.dsclientes.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,10 +20,16 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public ClientDTO findByIdClientDTO(Long id) {
+	public ClientDTO findById(Long id) {
 		Client client = repository.findById(id).get();
-		return new ClientDTO(client);
-		 
+		return new ClientDTO(client);	 
+		
+	}
+	
+	@Transactional
+	public Page<ClientDTO> findAll(Pageable pageable) {
+		Page<Client> result = repository.findAll(pageable);
+		return result.map(x -> new ClientDTO(x));	 
 		
 	}
 }
