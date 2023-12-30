4c485dc4bdc9c0adb2077b7912c082d5191abe47
ROBERTA, Karol <karolroberta92@hotmail.com>
2023-12-30 03:47:41 -0300

    build: Delete client

--- clientes/src/main/java/com/dsclientes/controllers/ClientController.java
+++ clientes/src/main/java/com/dsclientes/controllers/ClientController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,5 +59,10 @@ public class ClientController {
 		 
 	}
 	
-	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		 
+	}
 }
